ec2d4a4fefd81b4c7aa51e5c15b80332eec4c77d
Hendrik Schmitt <hendrik.schmitt@hhu.de>
2022-06-01 09:35:03 +0200

    Update tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,1 +1,17 @@
 # This will be the tokenizer
+import re
+
+
+def get_token_from_line(line):
+    return [x.lower() for x in re.split(" |\"|\?|,|!|.|&", line) if x != '']
+
+
+def tokenize_documents(documents):
+    tokens = []
+    for i in range(len(documents)):
+        with open(documents[i]) as doc:
+            lines = doc.readlines()
+            for line in lines:
+                new_tokens = get_token_from_line(line)
+                for new_token in new_tokens:
+                    tokens.append(new_token)
