e585b8fbe7e69c0530866ca7c089be8e5d8711fd
Max Ibbels <maxibbels55@gmail.com>
2022-06-01 17:49:09 +0200

    Added Indexer + to_json() . Update tokenizer
    Update tokenizer to fit specific file schema of ID.txt

---
+++ indexer.py
@@ -0,0 +1,86 @@
+from tokenizer import tokenize_documents
+import json
+
+
+class Index:
+    def __init__(self, filename: str):
+        # dict[TermIndex, Postinglist]
+        self.idx = {}
+        self.termClassMapping = {}   # sollte auch bei index anfragen verwendet werden müssen
+        # sort term, dann sort docid
+        # sortieren term überspringen, da kein unterschied in dict?
+
+        self.invoke_toknizer(filename)
+
+    def invoke_toknizer(self, filename: str):
+        try:
+            with open(filename, 'r', encoding='utf8') as f:
+                file = f.read()
+                docs = file.split('\n')
+        except FileNotFoundError:
+            print(f'error opening file {filename}')
+            return
+
+
+        for docID, tokens in tokenize_documents(docs):
+            for token in tokens:
+                try:
+                    ti = self.termClassMapping[token]
+                except KeyError:
+                    ti = TermIndex(token)
+                    self.termClassMapping[token] = ti
+
+                try:
+                    self.idx[ti].append(docID)
+                    ti.occurence += 1
+                except KeyError:
+                    self.idx[ti] = Postinglist(token)
+
+        for key, val in self.idx.items():
+            val.final_sort()
+
+    def to_json(self):
+        obj = {}
+        for key, val in self.idx.items():
+            obj.update({key.term:val.plist[1:10]})
+        with open('index.json', 'w') as f:
+            json.dump(obj, f)
+
+    def from_json(self, filename: str):
+        pass
+
+
+
+class TermIndex:
+    __slots__ = ('term', 'occurence')  # membervars hier hinzufügen
+
+    def __init__(self, term: str):
+        self.term = term
+        self.occurence = 1
+
+    def __hash__(self):
+        return hash(self.term)
+
+
+class Postinglist:
+    __slots__ = ('plist', 'seenDocIDs')  # membervars hier hinzufügen
+
+    def __init__(self, docID: str):
+        # array.array
+        # numpy array
+        # oder liste
+        self.plist = []
+        self.seenDocIDs = set()
+        # Sortierung!
+
+        self.append(docID)
+
+    def append(self, docID: str):
+        if docID in self.seenDocIDs:
+            pass
+        else:
+            self.plist.append(docID)
+            self.seenDocIDs.add(docID)
+
+    def final_sort(self):
+        self.plist = sorted(self.plist)
--- tokenizer.py
+++ tokenizer.py
@@ -1,49 +1,60 @@
-import re, os
-
+import re, os, sys
+from indexer import Index
 
 def get_token_from_line(line):
     return [x.lower() for x in re.split("[ |\"|\?|,|!|.|&|\n|;|:|...|-|\\\|/|(|)|\[|\]]", line) if x != '']
 
-
-def tokenize_documents(documents):
-    tokens = []
+def tokenize_documents(documents: list[str]):
+    """    tokens = []
     for i in range(len(documents)):
         with open(documents[i]) as doc:
-            lines = doc.readlines()
-            for line in lines:
-                new_tokens = get_token_from_line(line)
-                for new_token in new_tokens:
-                    tokens.append(new_token)
+            lines = doc.readlines()"""
 
-    return tokens
+    for line in documents:
+        tmp = re.split(r'\$\$\$', line)
+        if len(tmp) != 2: # warum taucht leerstring auf?
+            #print(tmp)
+            continue
+        docID, text = tmp[0], tmp[1]
+        new_tokens = get_token_from_line(text)
+        yield docID, new_tokens
+                #for new_token in new_tokens:
+                    #tokens.append(new_token)
 
-# nfcorpus/raw/doc_dump.txt
-# Format pro Zeile ID \t URL \t TITLE \t ABSTRACT \r\n
-    # ID und ABSTRACT extrahieren
-    # speichern unter ID.txt
+    #return tokens
+
+
 
 
 def parse_doc_dump():
-    doc_dump_path = os.path.join(os.getcwd(), 'nfcorpus', 'raw', 'doc_dump.txt')
+    # nfcorpus/raw/doc_dump.txt
+    # Format pro Zeile ID \t URL \t TITLE \t ABSTRACT \r\n
+    # ID und ABSTRACT extrahieren
+    # speichern unter ID.txt
+
+    docdumpPath = os.path.join(os.getcwd(), 'nfcorpus', 'raw', 'doc_dump.txt')
     newDocument = ''
     try:
-        with open(doc_dump_path, 'r', encoding='utf8') as f:
+        with open(docdumpPath, 'r', encoding='utf8') as f:
             file = f.read()
             file = file.split('\n')
     except FileNotFoundError:
-        print(f'error opening file {doc_dump_path}')
+        print(f'error opening file {docdumpPath}')
         return
 
     for line in file:
         docContent = re.split(r'\t', line)
         ID, abstract = docContent[0], docContent[3]
-        abstract = re.sub(r'^Abstract', '', abstract)
+        abstract = re.sub(r'^Abstract', '', abstract).strip()
 
-        newDocument += (ID + ' ' + abstract + '\n')
+        newDocument += (ID + '$$$' + abstract + '\n')
 
     with open('ID.txt', 'w', encoding='utf8') as f:
         f.write(newDocument)
 
 if __name__ == '__main__':
+    # creates ID.txt
     #parse_doc_dump()
-    pass
+
+    i = Index('ID.txt')
+    i.to_json()
