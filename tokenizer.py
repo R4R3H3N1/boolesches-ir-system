38c50155e231daf5ecfaf4b0d825877a2ed5fb37
Max Ibbels <maxibbels55@gmail.com>
2022-06-01 15:31:42 +0200

    update gitignore, parse_doc_dump()
    parse_doc_dump() -> transform raw/doc_dump.txt into ID.txt

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,4 @@
-# This will be the tokenizer
-import re
+import re, os
 
 
 def get_token_from_line(line):
@@ -17,3 +16,34 @@ def tokenize_documents(documents):
                     tokens.append(new_token)
 
     return tokens
+
+# nfcorpus/raw/doc_dump.txt
+# Format pro Zeile ID \t URL \t TITLE \t ABSTRACT \r\n
+    # ID und ABSTRACT extrahieren
+    # speichern unter ID.txt
+
+
+def parse_doc_dump():
+    doc_dump_path = os.path.join(os.getcwd(), 'nfcorpus', 'raw', 'doc_dump.txt')
+    newDocument = ''
+    try:
+        with open(doc_dump_path, 'r', encoding='utf8') as f:
+            file = f.read()
+            file = file.split('\n')
+    except FileNotFoundError:
+        print(f'error opening file {doc_dump_path}')
+        return
+
+    for line in file:
+        docContent = re.split(r'\t', line)
+        ID, abstract = docContent[0], docContent[3]
+        abstract = re.sub(r'^Abstract', '', abstract)
+
+        newDocument += (ID + ' ' + abstract + '\n')
+
+    with open('ID.txt', 'w', encoding='utf8') as f:
+        f.write(newDocument)
+
+if __name__ == '__main__':
+    #parse_doc_dump()
+    pass
